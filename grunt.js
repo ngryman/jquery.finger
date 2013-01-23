0308c6dc852e3ec392d0826aa2a5610c931a4b41
Nicolas Gryman <ngryman@gmail.com>
2013-01-22 23:32:48 -0500

    config: added grunt test task.

--- grunt.js
+++ grunt.js
@@ -56,5 +56,6 @@ module.exports = function(grunt) {
 
     // Default task.
     grunt.registerTask('default', 'lint qunit concat min');
+    grunt.registerTask('test', 'lint qunit');
     grunt.registerTask("run", "server watch");
 };
